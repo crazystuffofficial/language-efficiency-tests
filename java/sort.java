5e4dd61129eabd4aa77a6b8e8514f2eb714b1338
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2025-01-02 16:34:11 -0500

    Add files via upload

---
+++ java/sort.java
@@ -0,0 +1,39 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import com.sun.management.OperatingSystemMXBean;
+import java.util.Arrays;
+import java.util.Collections;
+public class Sort {
+    public static void main(String[] args) throws IOException {
+        int size = 1_000_000;
+        Integer[] arr = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i + 1;
+        }
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage beforeUsage = memoryMXBean.getHeapMemoryUsage();
+        long usedMemoryBefore = beforeUsage.getUsed();
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        long startCpuTime = osBean.getProcessCpuTime();
+        long startTime = System.nanoTime();
+        Arrays.sort(arr, Collections.reverseOrder());
+        long endTime = System.nanoTime();
+        MemoryUsage afterUsage = memoryMXBean.getHeapMemoryUsage();
+        long usedMemoryAfter = afterUsage.getUsed();
+        long endCpuTime = osBean.getProcessCpuTime();
+        long elapsedCpuTime = endCpuTime - startCpuTime;
+        double elapsedTime = endTime - startTime;
+        double cpuUsage = elapsedCpuTime / elapsedTime * 100;
+        try (FileWriter log = new FileWriter("log.txt", true)) {
+            log.write("Java:\n");
+            log.write("Execution time: " + elapsedTime/1_000_000 + " ms\n");
+            log.write("Memory usage change: " + (usedMemoryAfter - usedMemoryBefore) / 1024 + " KB\n");
+            log.write(String.format("CPU usage: %.2f%%\n\n", cpuUsage));
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
+}
---
+++ python/sort.py
@@ -0,0 +1,19 @@
+import time
+import psutil
+import os
+if __name__ == "__main__":
+    arr = list(range(1, 1_000_001))
+    process = psutil.Process(os.getpid())
+    start_cpu_percent = psutil.cpu_percent(interval=None)
+    start_time_ns = time.perf_counter_ns()
+    start_memory = process.memory_info().rss
+    arr = sorted(arr, reverse=True)
+    end_time_ns = time.perf_counter_ns()
+    end_memory = process.memory_info().rss
+    end_cpu_percent = psutil.cpu_percent(interval=None)
+    execution_time = (end_time_ns - start_time_ns) / 1_000_000
+    with open("log.txt", "a") as log:
+        log.write("Python: \n")
+        log.write(f"Execution time: {execution_time:.9f} ms\n")
+        log.write(f"Memory usage change: {(end_memory - start_memory) / 1024:.2f} KB\n")
+        log.write(f"CPU usage: {end_cpu_percent:.2f}%\n\n")
